d2b3d07e2d70a65d439aeb99719b08b32dac4763
GuilaneDen <guilane@blooo.io>
2021-11-29 10:24:43 +0100

    feat(repo): Changed all the repository with working examples

--- basic-carlo-webusb/main.js
+++
@@ -1,9 +0,0 @@
-const carlo = require("carlo");
-const path = require("path");
-
-(async () => {
-  const app = await carlo.launch();
-  app.on("exit", () => process.exit());
-  app.serveFolder(path.join(__dirname, "dist"));
-  await app.load("index.html");
-})();
